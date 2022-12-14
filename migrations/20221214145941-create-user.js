533e6e76c5c7ae0d7e9c2736cb810ba98cbee9c8
Aditya <adityaagr.201ec103@gmail.com>
2022-12-14 20:46:23 +0530

    created user migration and model

---
+++ migrations/20221214145941-create-user.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      role: {
+        type: Sequelize.STRING,
+        defaultValue: 'member'
+      },
+      image_url: {
+        type: Sequelize.STRING
+      },
+      instagram: {
+        type: Sequelize.STRING
+      },
+      gmail: {
+        type: Sequelize.STRING,
+      },
+      linkedin: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    name: {
+      type: DataTypes.STRING,
+      validate: {notEmpty: true}
+    },
+    role: DataTypes.STRING,
+    image_url: DataTypes.STRING,
+    instagram: DataTypes.STRING,
+    gmail: {
+      type: DataTypes.STRING,
+      validate: {isEmail: true}
+    },
+    linkedin: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
