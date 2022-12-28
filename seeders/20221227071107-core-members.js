5c709eb1566bf32950ba5ebef272c893464d35f8
s27ana <sanaazmia@gmail.com>
2022-12-28 21:44:09 +0530

    added core members to database

---
+++ seeders/20221227071107-core-members.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const fs = require('fs');
+
+const data = fs.readFileSync('mongo-exports/teams.json', {encoding: 'utf8', flag: 'r'});
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    await queryInterface.bulkInsert('Users', JSON.parse(data) , {});
+
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+
+    await queryInterface.bulkDelete('Users', null, {});
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
