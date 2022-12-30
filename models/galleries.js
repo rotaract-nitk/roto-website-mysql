be64443d294dc220810808b16c9759d9edce7f25
Venkat Kumar <venkatkumaryenumula18@gmail.com>
2022-12-30 12:29:06 +0530

    fixed models gallery and user

--- models/galleries.js
+++ models/galleries.js
@@ -19,6 +19,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Galleries',
+    underscored: true
   });
   return Galleries;
 };
--- models/user.js
+++ models/user.js
@@ -29,6 +29,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    underscored: true
   });
   return User;
 };
